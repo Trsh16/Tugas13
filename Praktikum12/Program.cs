48c49fcf0b1a3613305019818c91c49f74f51fb2
Trsh16 <tlikorawung@student.ciputra.ac.id>
2021-12-11 17:11:02 +0800

    new

--- Praktikum12/Program.cs
+++ Praktikum12/Program.cs
@@ -10,42 +10,51 @@ namespace Praktikum12
     {
         static void Main()
         {
-            Console.WriteLine("1. My scroll list\n2. Add scroll\nChoose what to do: ");
-            int input = Convert.ToInt32(Console.ReadLine());
-            if (input == 1)
+            var input = 0;
+            var counter = 0;
+            var scroll = 0;
+            var queue = 0;
+            List<string> baru = new List<string>();
+            List<string> scrolls = new List<string>() { "Book of Peace", "Scroll of Swords", "Silence Guide Book" };
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Scroll to learn list:");
-                List<string> scrolls = new List<string>() { "Book of Peace", "Scroll of Swords", "Silence Guide Book" };
-                scrolls.InsertRange(2, new string[] {"book 3","book 4"});
-                for (int i = 0; i < 5; i++)
+                Console.Write("1. My scroll list\n2. Add scroll\nChoose what to do : ");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 1)
                 {
-                    foreach (string scroll in scrolls)
+                    Console.Clear();
+                    counter = 0;
+                    Console.WriteLine("Scroll to learn list : ");
+                    for (int i = 0; i < scrolls.Count(); i++)
                     {
-                        Console.WriteLine($"Scroll {i + 1}: " + scroll);
+                        Console.WriteLine($"Scroll {i + 1}: {scrolls[i]}");
                     }
                 }
-                Console.ReadKey();
-                return;
-            }
-            if (input == 2)
-            {
-                Console.Clear();
-                int[] scroll = new int[10];
-                int[] queue = new int[10];
-                for (int i = 0; i < 1; i++)
+                else if (input == 2)
                 {
-                    Console.WriteLine("How many scroll to add:");
-                    scroll[i] = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("In what number of queue?");
-                    queue[i] = Convert.ToInt32(Console.ReadLine());
-                    string[] name = new string[10];
-                    for (i = 0; i < 2; i++)
+                    Console.Clear();
+                    for (int i = 0; i < 1; i++)
                     {
-                        Console.WriteLine($"Scroll {i + 1} name: ");
-                        name[i] = Console.ReadLine();
-                    }
-                    return;
+                        Console.WriteLine("How many scroll to add:");
+                        scroll = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("In what number of queue?");
+                        queue = Convert.ToInt32(Console.ReadLine());
+                        for (i = 0; i < scroll; i++)
+                        {
+                            Console.WriteLine($"Scroll {i + 1} name: ");
+                            baru.Add(Console.ReadLine());
+                        }
+                        if (queue > scrolls.Count())
+                        {
+                            queue = scrolls.Count();
+                        }
+                        foreach (var scrool in baru)
+                        {
+                            scrolls.Insert(queue + counter - 1, scrool);
+                            counter++;
+                        }
+                        baru.Clear();
+                    } 
                 }
             }
         }
